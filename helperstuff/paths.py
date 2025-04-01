2e242d4ec3c23ff4357d97429d2cce2d8ad96c8e
Martina Manoni <m.manoni2@campus.unimib.it>
2025-04-01 10:47:07 +0200

    solved conflicts

--- helperstuff/paths.py
+++ helperstuff/paths.py
@@ -1,24 +1,15 @@
 ### ------- lxplus -------
 path = {
- #'eos_path_sig': '/eos/user/a/atarabin/MC_samples/',
- #'eos_path': '/eos/user/a/atarabin/',
- #'eos_path_FR': '/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/'
-
- 'eos_path_sig': '/eos/home-s/sellissp/HZZ/SAMPLES/032025/',
- 'eos_path': '/eos/user/s/sellissp',
- 'eos_path_FR': '/eos/home-s/sellissp/HZZ/SAMPLES/032025/',
-
- #'eos_path_sig': '/eos/user/l/lurda/CMS/HZZ/XS_analysis/250303/',
- #'eos_path': '/eos/user/s/sellissp',
- #'eos_path_FR': '/eos/user/l/lurda/CMS/HZZ/XS_analysis/250303/',
-    
+'eos_path_sig': '/eos/user/m/mmanoni/HZZ_FIDUCIAL/',
+'eos_path': '/eos/user/m/mmanoni/',
+'eos_path_FR': '/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIII_byZ1Z2/240820/'
 }
 
 
 ### ------- polui UL -------
 ## To get ReReco path, remove MC_samples_UL
-#path = {
-#'eos_path_sig': '/grid_mnt/data__data.polcms/cms/tarabini/HZZ4l/MC_samples_UL/',  ## If you set to ReReco, change from %i_MELA to MC_samples/%i_MELA
-#'eos_path': '/grid_mnt/data__data.polcms/cms/tarabini/HZZ4l/',
-#'eos_path_FR': '/grid_mnt/data__data.polcms/cms/tarabini/HZZ4l/', ## In RunTemplated if you set to  ReReco change name from FakeRates_SS_%i.root to newData_FakeRates_SS_%i.root
-#}
+##path = {
+##'eos_path_sig': '/grid_mnt/data__data.polcms/cms/tarabini/HZZ4l/MC_samples_UL/',  ## If you set to ReReco, change from %i_MELA to MC_samples/%i_MELA
+##'eos_path': '/grid_mnt/data__data.polcms/cms/tarabini/HZZ4l/',
+##'eos_path_FR': '/grid_mnt/data__data.polcms/cms/tarabini/HZZ4l/', ## In RunTemplated if you set to  ReReco change name from FakeRates_SS_%i.root to newData_FakeRates_SS_%i.root
+##}
