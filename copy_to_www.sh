a0b03e13b3c75788c921b65212a0fc6b9bdcc037
Martina Manoni <m.manoni2@campus.unimib.it>
2025-05-12 17:03:57 +0200

    Pipeline Martina

--- copy_to_www.sh
+++ copy_to_www.sh
@@ -1,95 +1,88 @@
 #!/bin/bash
+# How to Run: ./copy_to_www.sh costhetaZ1 2022EE
+# Works on lxplus 
 
-#Check if the name of the observable is provided in the command line
-if [[ $# -eq 0 ]] ; then
-    echo 'no name of the observable is provided'
+# Check if observable and year are provided
+if [[ $# -lt 2 ]] ; then
+    echo "Usage: $0 <observable> <year>"
+    echo "Example: $0 costhetaZ1 2022"
+    echo "Optional: Provide 'overwrite' as the third argument to overwrite only the year-specific directory."
     exit 1
 fi
 
-# path="/eos/user/a/atarabin/www/fiducial/run2_cardsValidation" #ReReco
-path="/eos/user/a/atarabin/www/fiducial/run2_UL_cardsValidation" #UL
+observable=$1
+year=$2
+overwrite_year=$3 
 
-/opt/exp_soft/cms/t3/eos-login -init -username atarabin
+path="/eos/user/m/mmanoni/www/HZZ_Fiducial/" #Run3
 
-cd coefficients
-source /opt/exp_soft/llr/root/vv6.20.06-el7-gcc9-py37/etc/init.sh
-python RunPlotCoefficients.py --obsName "${1}" --year 'Full'
-cd ../fit
-python RunPlotCorrelation.py --obsName "${1}" --year 'Full'
-cd ..
-
-if [ -d "$path/${1}" ]; then
-  ### Take action if $DIR exists ###
-  rm -r $path/${1}
-  mkdir $path/${1}
-  cp $path/index.php $path/${1}
+# Check if observable directory exists
+if [ -d "$path/${observable}" ]; then
+  echo "Observable directory exists: $observable"
 else
-  ###  Control will jump here if $DIR does NOT exists ###
-  mkdir $path/${1}
-  cp $path/index.php $path/${1}
+  # Observable directory does not exist, create it
+  mkdir $path/${observable}
+  cp $path/index.php $path/${observable}
 fi
 
-mkdir $path/${1}/datacard_2016 $path/${1}/datacard_2017 $path/${1}/datacard_2018
-cp $path/index.php $path/${1}
-cp $path/index.php $path/${1}/datacard_2016
-cp $path/index.php $path/${1}/datacard_2017
-cp $path/index.php $path/${1}/datacard_2018
+mkdir $path/${observable}/datacard_${year}
+cp $path/index.php $path/${observable}
+cp $path/index.php $path/${observable}/datacard_${year}
 
-mkdir $path/${1}/templatesBkgs_2016 $path/${1}/templatesBkgs_2017 $path/${1}/templatesBkgs_2018
-cp $path/index.php $path/${1}/templatesBkgs_2016/.
-cp $path/index.php $path/${1}/templatesBkgs_2017/.
-cp $path/index.php $path/${1}/templatesBkgs_2018/.
-cp templates/plots/2016/${1}/* $path/${1}/templatesBkgs_2016/.
-cp templates/plots/2017/${1}/* $path/${1}/templatesBkgs_2017/.
-cp templates/plots/2018/${1}/* $path/${1}/templatesBkgs_2018/.
+mkdir $path/${observable}/templatesBkgs_${year}
+cp $path/index.php $path/${observable}/templatesBkgs_${year}/.
+# Move tempaltes plots
+cp templates/plots/${year}/${observable}/* $path/${observable}/templatesBkgs_${year}/.
 
-cp coefficients/matrix_eff/2016/eff_2016_${1}_* $path/${1}/.
-cp coefficients/matrix_eff/2017/eff_2017_${1}_* $path/${1}/.
-cp coefficients/matrix_eff/2018/eff_2018_${1}_* $path/${1}/.
+mkdir $path/${observable}/eff_${year}
+mkdir $path/${observable}/nonfid_${year}
 
-cp coefficients/matrix_nonfid/2016/nonFid_2016_${1}_* $path/${1}/.
-cp coefficients/matrix_nonfid/2017/nonFid_2017_${1}_* $path/${1}/.
-cp coefficients/matrix_nonfid/2018/nonFid_2018_${1}_* $path/${1}/.
+cp $path/index.php $path/${observable}/eff_${year}
+cp $path/index.php $path/${observable}/nonfid_${year}
+# Move coefficients plots
+cp coefficients/matrix_eff/${year}/eff_${year}_${observable}_* $path/${observable}/eff_${year}/.
+cp coefficients/matrix_nonfid/${year}/nonFid_${year}_${observable}_* $path/${observable}/nonfid_${year}/.
 
 # Move datacards
-if [ ${1} = "njets_pt30_eta4p7" ]; then
+if [ ${observable} = "njets_pt30_eta4p7" ]; then
   obs_datacards="NJ"
-elif [ ${1} = "pT4l" ]; then
+elif [ ${observable} = "pT4l" ]; then
   obs_datacards="PTH"
-elif [ ${1} = "rapidity4l" ]; then
+elif [ ${observable} = "rapidity4l" ]; then
   obs_datacards="YH"
-elif [ ${1} = "pTj1" ]; then
+elif [ ${observable} = "pTj1" ]; then
   obs_datacards="PTJET"
 else
-  obs_datacards=${1}
+  obs_datacards=${observable}
 fi
 
-cp datacard/datacard_2016/hzz4l_*_13TeV_xs_${obs_datacards}_bin*_* $path/${1}/datacard_2016/.
-cp datacard/datacard_2017/hzz4l_*_13TeV_xs_${obs_datacards}_bin*_* $path/${1}/datacard_2017/.
-cp datacard/datacard_2018/hzz4l_*_13TeV_xs_${obs_datacards}_bin*_* $path/${1}/datacard_2018/.
-# Move ws
+cp datacard/datacard_${year}/hzz4l_*_13TeV_xs_${obs_datacards}_bin*_* $path/${obs_datacards}/datacard_${year}/.
 if [ ${1} = "pT4l_kL" ]; then
-  cp datacard/datacard_2016/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_kLambda* $path/${1}/datacard_2016/.
-  cp datacard/datacard_2017/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_kLambda* $path/${1}/datacard_2017/.
-  cp datacard/datacard_2018/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_kLambda* $path/${1}/datacard_2018/.
-  cp datacard/hzz4l_all_13TeV_xs_${1}_bin_kLambda* $path/${1}/.
+  cp datacard/datacard_${year}/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_kLambda* $path/${observable}/datacard_${year}/.
+  cp datacard/hzz4l_all_13TeV_xs_${observable}_bin_kLambda* $path/${observable}/datacard_${year}/.
 else
-  cp datacard/datacard_2016/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_v* $path/${1}/datacard_2016/.
-  cp datacard/datacard_2017/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_v* $path/${1}/datacard_2017/.
-  cp datacard/datacard_2018/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_v* $path/${1}/datacard_2018/.
-  cp datacard/hzz4l_all_13TeV_xs_${1}_bin_v* $path/${1}/.
+  cp datacard/datacard_${year}/hzz4l_*_13TeV_xs_SM_125_${obs_datacards}_v* $path/${observable}/datacard_${year}/.
+  cp datacard/hzz4l_all_13TeV_xs_${observable}_bin_v* $path/${observable}/datacard_${year}/.
 fi
 
-cp plots/${1}/asimov/${1}_unfoldwith* $path/${1}/.
-cp plots/${1}/asimov/corr_${1}_*.png $path/${1}/.
-cp plots/${1}/asimov/model/* $path/${1}/.
+mkdir $path/${observable}/LHScans_${year}
+cp $path/index.php $path/${observable}/LHScans_${year}
 if [ ${1} = "pT4l_kL" ]; then
-  cp impacts/${1}/impacts_v3_kappa_lambda* $path/${1}/.
-  cp LHScans/plots/lhscan_compare_${1}_kappa* $path/${1}/.
+  cp LHScans/plots/${year}_lhscan_compare_${observable}_kappa* $path/${observable}/LHScans_${year}/.
 else
-  cp impacts/${1}/impacts_*_${1}_*_asimov* $path/${1}/.
-  cp LHScans/plots/lhscan_compare_${1}_r* $path/${1}/.
+  cp LHScans/plots/${year}_lhscan_compare_${observable}_r* $path/${observable}/LHScans_${year}/.
 fi
 
-cp fit/commands_${1}.py $path/${1}/.
-cp impacts/${1}/commands_impacts_${1}_v* $path/${1}/.
+#cp plots/${1}/asimov/${1}_unfoldwith* $path/${1}/.
+#cp plots/${1}/asimov/corr_${1}_*.png $path/${1}/.
+#cp plots/${1}/asimov/model/* $path/${1}/.
+#if [ ${1} = "pT4l_kL" ]; then
+# cp impacts/${1}/impacts_v3_kappa_lambda* $path/${1}/.
+# cp LHScans/plots/lhscan_compare_${1}_kappa* $path/${1}/.
+#else
+# cp impacts/${1}/impacts_*_${1}_*_asimov* $path/${1}/.
+# cp LHScans/plots/lhscan_compare_${1}_r* $path/${1}/.
+#fi
+
+#cp fit/commands_${1}.py $path/${1}/.
+#cp impacts/${1}/commands_impacts_${1}_v* $path/${1}/.
