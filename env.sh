58df6143603237d21bc4ab7714cf1361343237b3
Martina Manoni <m.manoni2@campus.unimib.it>
2025-05-28 17:47:25 +0200

    Update pipeline + add submission set up in README

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 export base_path=$PWD
+export transfer_input_files=$PWD/coefficients/RunCoefficients.py
 echo $base_path;
 cd LHScans; mkdir plots;
 cd $base_path; 
@@ -7,7 +8,7 @@ mkdir datacard; cd datacard; mkdir datacard_2022; mkdir datacard_2022EE;
 mkdir datacard_2023postBPix; mkdir datacard_2023preBPix;
 cd $base_path;
 mkdir plots;
-cd templates; mkdir 2022; mkdir 2023;
+cd templates; mkdir 2022; mkdir 2022EE; mkdir 2023preBPix; mkdir 2023postBPix;
 cd $base_path;
 mkdir scripts;
 cd $base_path;
